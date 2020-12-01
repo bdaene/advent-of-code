08406a477f7f14f76effb3889bd1c2d9ac8dc258
bdaene <benoitdaene@gmail.com>
2020-12-01 09:16:31 +0100

    Add 2020 day 1 solution

---
+++ 2020/day 1/solve.py
@@ -0,0 +1,25 @@
+def part_1(data):
+    for i, expense in enumerate(data):
+        expense_2 = 2020 - expense
+        if expense_2 in data[i + 1:]:
+            print(expense, expense_2, expense * expense_2)
+
+
+def part_2(data):
+    for i, expense_1 in enumerate(data):
+        for j, expense_2 in enumerate(data[i + 1:], i + 1):
+            expense_3 = 2020 - expense_1 - expense_2
+            if expense_3 in data[j + 1:]:
+                print(expense_1, expense_2, expense_3, expense_1 * expense_2 * expense_3)
+
+
+def main():
+    with open('input.txt') as input_file:
+        data = [int(entry) for entry in input_file]
+
+    part_1(data)
+    part_2(data)
+
+
+if __name__ == "__main__":
+    main()
