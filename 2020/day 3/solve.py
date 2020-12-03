ecd3c8b3a1d7dcf016d4646f5716948f97c2ef0c
bdaene <benoitdaene@gmail.com>
2020-12-03 07:43:08 +0100

    Add 2020 day 3 solution

---
+++ 2020/day 3/solve.py
@@ -0,0 +1,39 @@
+def part_1(trees):
+    position = 0
+    count = 0
+    for line in trees[1:]:
+        position += 3
+        if line[position % len(line)] == '#':
+            count += 1
+    print(count)
+
+
+def part_2(trees):
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    counts = []
+    total = 1
+
+    for r, d in slopes:
+        position = 0
+        count = 0
+        for line in trees[d::d]:
+            position += r
+            if line[position % len(line)] == '#':
+                count += 1
+        counts.append(count)
+        total *= count
+
+    print(counts)
+    print(total)
+
+
+def main():
+    with open('input.txt') as input_file:
+        trees = [line.strip() for line in input_file]
+
+    part_1(trees)
+    part_2(trees)
+
+
+if __name__ == "__main__":
+    main()
