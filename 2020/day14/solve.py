274853ee3e04aa227c708f10e6cb3885a64af1dc
bdaene <benoitdaene@gmail.com>
2020-12-15 15:40:02 +0100

    Improve 2020 day 14 solution: extract memory trie in a class.

--- 2020/day14/solve.py
+++ 2020/day14/solve.py
@@ -126,36 +126,8 @@ def part_2_bis(data, size=36):
 
 @timeit
 def part_2_ter(data, size=36):
-    memory = {}
+    memory = Memory(size)
     mask = '0' * size
-
-    used_addresses = {}
-
-    def add_address(address):
-        node = used_addresses
-        for a in address:
-            if a not in node:
-                node[a] = {}
-            node = node[a]
-        node[address] = '$'
-
-    def get_addresses(address):
-        stack = [used_addresses]
-        for a in address:
-            stack_ = []
-            for node in stack:
-                if a == 'X':
-                    stack_.extend(node.values())
-                else:
-                    if a in node:
-                        stack_.append(node[a])
-                    if 'X' in node:
-                        stack_.append(node['X'])
-            stack = stack_
-        addresses = []
-        for node in stack:
-            addresses.extend(node)
-        return addresses
 
     for action in data:
         if action[0] == 'mask':
@@ -164,26 +136,64 @@ def part_2_ter(data, size=36):
 
         address, value = action[1:]
         address = apply_mask(address, mask)
+        memory.set(address, value)
 
-        for add in get_addresses(address):
-            if add not in memory:
-                continue
-            val = memory.pop(add)
-            for i, b in enumerate(address):
-                if add[i] == b or b == 'X':
-                    continue
-                add_0 = add[:i] + address[i] + add[i + 1:]
-                add_1 = add[:i] + ('1' if address[i] == '0' else '0') + add[i + 1:]
+    return sum(value * 2 ** (address.count('X')) for address, value in memory.get('X' * size))
 
-                memory[add_1] = val
-                add_address(add_1)
-                add = add_0
 
-        memory[address] = value
-        add_address(address)
+class Memory:
+    def __init__(self, size):
+        self.root = {}  # Trie on characters '01X' no overlapping addresses allowed
+        self.size = size  # Depth of the trie
 
-    print(len(memory))
-    return sum(value * 2 ** (address.count('X')) for address, value in memory.items())
+    def get(self, address):
+        """Get all stored addresses overlapping with given address."""
+        assert len(address) == self.size
+        nodes = [self.root]
+        for a in address:
+            nodes_ = []
+            for node in nodes:
+                if a == 'X':
+                    nodes_.extend(node.values())
+                else:
+                    if a in node:
+                        nodes_.append(node[a])
+                    if 'X' in node:
+                        nodes_.append(node['X'])
+            nodes = nodes_
+        return nodes
+
+    def set(self, address, value):
+        """Set the address to the value. Erasing all overlapping stored addresses."""
+        assert len(address) == self.size
+        for add, val in self.get(address):
+            self._remove(add)
+            for i, a in enumerate(add):
+                if a == 'X' and address[i] != 'X':
+                    self._store(add[:i] + ('1' if address[i] == '0' else '0') + add[i + 1:], val)
+                    add = add[:i] + address[i] + add[i + 1:]
+        self._store(address, value)
+
+    def _store(self, address, value):
+        """Store the value at the given address. Address must not overlap any stored addresses."""
+        node = self.root
+        for a in address[:-1]:
+            if a not in node:
+                node[a] = {}
+            node = node[a]
+        node[address[-1]] = (address, value)
+
+    def _remove(self, address):
+        """Remove the given address. Address must be in memory."""
+        node = self.root
+        path = []
+        for a in address:
+            path.append((node, a))
+            node = node[a]
+        for node, a in reversed(path):
+            del node[a]
+            if len(node) > 0:
+                break
 
 
 def main():
