09ac882f7f1c1d664c14ede8d8b62730f8677a8e
bdaene <benoitdaene@gmail.com>
2020-12-23 10:15:49 +0100

    Improve 2020 day 22 solution assuming that if we see the same cards in a deck (unordered) we have an infinite game.

--- 2020/day22/solve.py
+++ 2020/day22/solve.py
@@ -1,5 +1,6 @@
 from collections import deque
 
+from itertools import islice
 from utils import timeit
 
 
@@ -49,30 +50,35 @@ def part_2(decks):
             return True, None
 
         seen_decks = set()
+        # key = sum(1 << a for a in deck1)
+        key = 0
         while len(deck1) > 0 and len(deck2) > 0:
-            key = (deck1, deck2)
             if key in seen_decks:
                 return True, None
             seen_decks.add(key)
 
-            card1 = deck1[0]
-            card2 = deck2[0]
-            deck1 = deck1[1:]
-            deck2 = deck2[1:]
+            card1 = deck1.popleft()
+            card2 = deck2.popleft()
 
             if len(deck1) >= card1 and len(deck2) >= card2:
-                p1_win, cards = play(deck1[:card1], deck2[:card2], False)
+                sub_deck1 = deque(islice(deck1, card1), deck1.maxlen)
+                sub_deck2 = deque(islice(deck2, card2), deck2.maxlen)
+                p1_win, cards = play(sub_deck1, sub_deck2, False)
             else:
                 p1_win = card1 > card2
 
             if p1_win:
-                deck1 += (card1, card2)
+                deck1.append(card1)
+                deck1.append(card2)
+                key += 1 << card2
             else:
-                deck2 += (card2, card1)
+                deck2.append(card2)
+                deck2.append(card1)
+                key -= 1 << card1
 
         return len(deck1) > 0, deck1 if len(deck1) > 0 else deck2
 
-    game1, deck = play(*decks, True)
+    game1, deck = play(deque(decks[0]), deque(decks[1]), True)
 
     return sum((len(deck) - i) * card for i, card in enumerate(deck))
 
