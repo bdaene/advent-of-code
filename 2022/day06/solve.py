d637c3cd6b12ffdf5ede86fb58bcb2879f6723be
bdaene <benoitdaene@gmail.com>
2022-12-06 10:01:17 +0100

    Improve 2022 day 06 solution.

--- 2022/day06/solve.py
+++ 2022/day06/solve.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from utils import timeit
 
 
@@ -9,10 +11,20 @@ def get_data():
 
 @timeit
 def part_1(data, n=4):
-    i = 0
-    while len(set(data[i:i + n])) != n:
-        i += 1
-    return i + n
+    letters = Counter(data[:n])
+    count = len(letters)
+    if count == n:
+        return n
+
+    for i, (a, b) in enumerate(zip(data, data[n:]), n + 1):
+        if letters[b] == 0:
+            count += 1
+        letters[b] += 1
+        letters[a] -= 1
+        if letters[a] == 0:
+            count -= 1
+        if count == n:
+            return i
 
 
 @timeit
